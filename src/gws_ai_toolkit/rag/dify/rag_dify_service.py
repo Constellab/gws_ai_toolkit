e32e5d337aef2c11ed81724b9bec4f453829492d
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2025-10-09 12:33:13 +0000

    (fix)[Rag] fix import

--- src/gws_ai_toolkit/rag/dify/rag_dify_service.py
+++ src/gws_ai_toolkit/rag/dify/rag_dify_service.py
@@ -1,13 +1,12 @@
 from typing import Any, Generator, List, Optional, Union
 
-from gws_core import CredentialsDataOther, DifyChunkRecord
-
 from gws_ai_toolkit.rag.common.base_rag_service import BaseRagService
 from gws_ai_toolkit.rag.common.rag_models import (RagChatEndStreamResponse,
                                                   RagChatStreamResponse,
                                                   RagChunk, RagDocument)
+from gws_core import CredentialsDataOther
 
-from .dify_class import (DifyDatasetDocument, DifyMetadata,
+from .dify_class import (DifyChunkRecord, DifyDatasetDocument, DifyMetadata,
                          DifySendDocumentOptions,
                          DifySendEndMessageStreamResponse,
                          DifySendMessageStreamResponse,
