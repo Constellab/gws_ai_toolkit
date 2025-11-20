5976d66def2ddb60bde84c1e2207c925ddbd1eac
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2025-11-20 11:23:06 +0000

    (feat) export rag folder

--- src/gws_ai_toolkit/__init__.py
+++ src/gws_ai_toolkit/__init__.py
@@ -1,6 +1,7 @@
 from .core.agents import *
 from .core.table_item import TableItem
 from .core.utils import Utils
+from .rag import *
 from .stats import *
 
 __all__ = [
---
+++ src/gws_ai_toolkit/rag/__init__.py
@@ -0,0 +1,122 @@
+from .common.base_rag_app_service import BaseRagAppService
+from .common.base_rag_service import BaseRagService
+from .common.datahub_rag_app_service import DatahubRagAppService
+from .common.rag_app_service_factory import RagAppServiceFactory
+from .common.rag_enums import (RAG_COMMON_MAX_FILE_SIZE_MB,
+                               RAG_COMMON_SUPPORTED_EXTENSIONS,
+                               RagDocumentStatus, RagProvider,
+                               RagResourceSyncMode, RagSearchMethod)
+from .common.rag_models import (RagChatEndStreamResponse, RagChatSource,
+                                RagChatSourceChunk, RagChatStreamResponse,
+                                RagChunk, RagCredentials, RagDocument)
+from .common.rag_resource import RagResource
+from .common.rag_service_factory import RagServiceFactory
+from .common.tag_rag_app_service import TagRagAppService
+from .dify.dify_class import (DifyChunkDocument, DifyChunkRecord,
+                              DifyChunksResponse,
+                              DifyCreateDatasetMetadataRequest,
+                              DifyCreateDatasetMetadataResponse,
+                              DifyDatasetDocument, DifyDocumentChunk,
+                              DifyDocumentChunksResponse,
+                              DifyGetDatasetMetadataResponse,
+                              DifyGetDatasetMetadataResponseMetadata,
+                              DifyGetDocumentsResponse, DifyMetadata,
+                              DifySegment, DifySendDocumentOptions,
+                              DifySendDocumentResponse,
+                              DifySendEndMessageStreamResponse,
+                              DifySendMessageSource,
+                              DifySendMessageStreamResponse,
+                              DifyUpdateDocumentOptions,
+                              DifyUpdateDocumentsMetadataRequest,
+                              DifyUploadFile, DifyUploadFileResponse)
+from .dify.dify_send_file_to_knownledge_base import \
+    DifySendFileToKnownledgeBase
+from .dify.dify_service import DifyService
+from .dify.rag_dify_service import RagDifyService
+from .ragflow.rag_ragflow_service import RagRagFlowService
+from .ragflow.ragflow_class import (RagflowAskStreamResponse,
+                                    RagFlowCreateChatRequest,
+                                    RagFlowCreateDatasetRequest,
+                                    RagFlowCreateSessionRequest,
+                                    RagFlowUpdateChatRequest,
+                                    RagFlowUpdateDatasetRequest,
+                                    RagFlowUpdateDocumentOptions)
+from .ragflow.ragflow_send_file_to_dataset import RagFlowSendFileToDataset
+from .ragflow.ragflow_service import RagFlowService
+from .ragflow.ragflow_start_docker_compose import RagflowStartDockerCompose
+
+__all__ = [
+    # Common - Base classes
+    'BaseRagService',
+    'BaseRagAppService',
+    # Common - App services
+    'DatahubRagAppService',
+    'TagRagAppService',
+    # Common - Factories
+    'RagAppServiceFactory',
+    'RagServiceFactory',
+    # Common - Resource
+    'RagResource',
+    # Common - Models
+    'RagDocument',
+    'RagChunk',
+    'RagChatStreamResponse',
+    'RagChatSourceChunk',
+    'RagChatSource',
+    'RagChatEndStreamResponse',
+    'RagCredentials',
+    # Common - Enums and constants
+    'RagDocumentStatus',
+    'RagProvider',
+    'RagResourceSyncMode',
+    'RagSearchMethod',
+    'RAG_COMMON_SUPPORTED_EXTENSIONS',
+    'RAG_COMMON_MAX_FILE_SIZE_MB',
+    # Dify - Service classes
+    'DifyService',
+    'RagDifyService',
+    # Dify - Task classes
+    'DifySendFileToKnownledgeBase',
+    # Dify - Document classes
+    'DifyUpdateDocumentOptions',
+    'DifySendDocumentOptions',
+    'DifyDatasetDocument',
+    'DifySendDocumentResponse',
+    'DifyGetDocumentsResponse',
+    # Dify - Message classes
+    'DifySendMessageSource',
+    'DifySendMessageStreamResponse',
+    'DifySendEndMessageStreamResponse',
+    # Dify - Chunk classes
+    'DifyChunkDocument',
+    'DifySegment',
+    'DifyChunkRecord',
+    'DifyChunksResponse',
+    'DifyDocumentChunk',
+    'DifyDocumentChunksResponse',
+    # Dify - Upload classes
+    'DifyUploadFile',
+    'DifyUploadFileResponse',
+    # Dify - Metadata classes
+    'DifyMetadata',
+    'DifyCreateDatasetMetadataRequest',
+    'DifyCreateDatasetMetadataResponse',
+    'DifyUpdateDocumentsMetadataRequest',
+    'DifyGetDatasetMetadataResponseMetadata',
+    'DifyGetDatasetMetadataResponse',
+    # RagFlow - Service classes
+    'RagFlowService',
+    'RagRagFlowService',
+    # RagFlow - Task classes
+    'RagFlowSendFileToDataset',
+    'RagflowStartDockerCompose',
+    # RagFlow - Request classes
+    'RagFlowUpdateDocumentOptions',
+    'RagFlowCreateDatasetRequest',
+    'RagFlowUpdateDatasetRequest',
+    'RagFlowCreateChatRequest',
+    'RagFlowUpdateChatRequest',
+    'RagFlowCreateSessionRequest',
+    # RagFlow - Response classes
+    'RagflowAskStreamResponse',
+]
